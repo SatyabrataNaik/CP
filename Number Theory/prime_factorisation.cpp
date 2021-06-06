22efe63ee9ab9eb614936fe74c17f613da838a0a
Satya <satyabrata1729@gmail.com>
2021-06-06 07:15:14 +0530

    Add sieveFactor method

--- Number Theory/prime_factorisation.cpp
+++ Number Theory/prime_factorisation.cpp
@@ -2,22 +2,43 @@
 using namespace std;
 typedef vector<int> vi;
 
-const int maxn = 2e6 + 5;
+const int maxn = 2e7 + 5;
 vi primes;
+int smallest_prime_fac[maxn];
 bool composite[maxn];
 
 void generatePrimes(){
-	for(int i = 2; i < maxn; i++){
-		if(!composite[i]){
-			primes.push_back(i);
-		}
-		for(int j = 0; j < primes.size() && i * primes[j] < maxn; j++){
-			composite[i * primes[j]] = true;
-			if(i % primes[j] == 0){
-				break;
-			}
-		}	
-	}
+    for(int i = 2; i < maxn; i++){
+        if(!composite[i]) {
+            primes.push_back(i);
+            smallest_prime_fac[i] = i;
+        }
+        for(int j = 0; j < primes.size() && i * primes[j] < maxn; j++){
+            composite[i * primes[j]] = true;
+            smallest_prime_fac[i * primes[j]] = primes[j];
+            if(i % primes[j] == 0){
+                break;
+            }
+        }   
+    }
+}
+
+map<int, int> sieveFactor(int n) {
+    assert(n < maxn && n >= 2);
+    map<int, int> ans;
+
+    while (n != 1) {
+        int cur_fac = smallest_prime_fac[n];
+        debug(n, cur_fac);
+        int cur_cnt = 0;
+        while (n % cur_fac == 0) {
+            cur_cnt += 1;
+            n /= cur_fac;
+        }
+        ans[cur_fac] = cur_cnt;
+    }
+
+    return ans;
 }
 
 map<int, int> factorise(int n){
